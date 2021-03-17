9767b17574c5ffb4f1fd9a464682e39d783fc1d6
Simple <69445064+Simple2006@users.noreply.github.com>
2021-03-17 01:52:30 -0700

    Update startBuying.bat

--- startBuying.bat
+++ startBuying.bat
@@ -1,2 +1,2 @@
-"C:\Users\Vinay\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\Vinay\Desktop\TradingAlgorithms\stocklist.py"
+"C:\Users\Vinay\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\Vinay\Desktop\tradealgo\operations\buyShares.py"
 pause
