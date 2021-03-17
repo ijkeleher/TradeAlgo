c7be303a7c973cbe7d2b44680a55d5701143e589
Simple <69445064+Simple2006@users.noreply.github.com>
2021-03-17 01:50:36 -0700

    Update getToken.py

--- getData/getToken.py
+++ getData/getToken.py
@@ -45,19 +45,19 @@ browser.find_by_value("Answer a security question").first.click()
 # Answer the Security Questions.
 if browser.is_text_present('What is your paternal grandmother\'s first name?'):
     browser.find_by_id("secretquestion0").first.click()
-    browser.find_by_id('secretquestion0').first.fill('alwar')
+    browser.find_by_id('secretquestion0').first.fill('')
 
 elif browser.is_text_present('What is your father\'s middle name?'):
     browser.find_by_id("secretquestion0").first.click()
-    browser.find_by_id('secretquestion0').first.fill('babu')
+    browser.find_by_id('secretquestion0').first.fill('')
 
 elif browser.is_text_present('What was your high school mascot?'):
     browser.find_by_id("secretquestion0").first.click()
-    browser.find_by_id('secretquestion0').first.fill('viking')
+    browser.find_by_id('secretquestion0').first.fill('')
 
 elif browser.is_text_present('What was the name of your junior high school? (Enter only \'Dell\' for Dell Junior High School.)'):
     browser.find_by_id("secretquestion0").first.click()
-    browser.find_by_id('secretquestion0').first.fill('miller')
+    browser.find_by_id('secretquestion0').first.fill('')
 
 # Submit results
 browser.find_by_id('accept').first.click()
